d4536655799760b435abd14ae5cf1e715a2ca252
Vishnu Priya <vishnupriya@VishnuPriya.local>
2017-01-25 15:07:04 +0530

    First Commit

---
+++ sampleapp/sampleappPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace sampleapp
+{
+	public partial class sampleappPage : ContentPage
+	{
+		public sampleappPage()
+		{			
+			InitializeComponent();
+					
+		}
+
+		public void OnClicked(object sender, EventArgs e)
+		{ 
+		DisplayAlert("JenkinsDemo", "Welcome to Jenkins with Xamarin", "OK");
+		}
+	}
+}
+
